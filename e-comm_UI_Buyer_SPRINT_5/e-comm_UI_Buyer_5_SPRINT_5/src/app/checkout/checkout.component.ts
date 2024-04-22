d2de1340f4008dc80e70b39dc080caa22c9391bb
gitaafu <aafreenwork521@gmail.com>
2024-04-22 15:25:16 +0530

    refactored the admin ticket

--- e-comm_UI_Buyer_SPRINT_5/e-comm_UI_Buyer_5_SPRINT_5/src/app/checkout/checkout.component.ts
+++ e-comm_UI_Buyer_SPRINT_5/e-comm_UI_Buyer_5_SPRINT_5/src/app/checkout/checkout.component.ts
@@ -5,30 +5,30 @@ import { PaymentDto } from '../payment-dto';
 import { BuyerDto } from '../buyer-dto';
 import Swal from 'sweetalert2';
 import { PaymentService } from '../payment.service';
-
+ 
 function cardNumberValidator(control: AbstractControl): ValidationErrors | null {
-  const validCardNumber =8688965686868896 ;
+  const validCardNumber =4242424242424242 ;
   const enteredCardNumber = control.value.replace(/-/g, ''); // Remove dashes before validation
   if (enteredCardNumber.length !== 16 || parseInt(enteredCardNumber) !== validCardNumber) {
     return { invalidCardNumber: true };
   }
   return null;
 }
-
-
+ 
+ 
 function CVVValidator(control: AbstractControl): ValidationErrors | null {
   const validCVVNumber = 152;
   const enteredCVV = control.value;
-  
+ 
   // Check if CVV is numeric and has 3 digits
   if (!/^\d{3}$/.test(enteredCVV) || parseInt(enteredCVV) !== validCVVNumber) {
     return { invalidCVV: true };
   }
-  
+ 
   return null;
 }
-
-
+ 
+ 
 @Component({
   selector: 'app-checkout',
   templateUrl: './checkout.component.html',
@@ -42,67 +42,67 @@ export class CheckoutComponent implements OnInit{
     const cardNumber = input.replace(/(.{4})/g, '$1-'); // Add a dash every 4 characters
     this.checkoutForm.patchValue({ cardNumber }); // Update the form value
   }
-
-
-
+ 
+ 
+ 
   months: string[] = [
     'January', 'February', 'March', 'April',
     'May', 'June', 'July', 'August',
     'September', 'October', 'November', 'December'
   ];
   years: number[] = [];
-  
-
-  
+ 
+ 
+ 
   totalCartValue!: number ;
   payment: PaymentDto = new PaymentDto();
   buyerDto: BuyerDto = new BuyerDto();
  
   // constructor(private route: ActivatedRoute, private paymentService: PaymentService) {
-    constructor(private route: ActivatedRoute, private formBuilder: FormBuilder, private paymentService: PaymentService, private router: Router) { 
+    constructor(private route: ActivatedRoute, private formBuilder: FormBuilder, private paymentService: PaymentService, private router: Router) {
       const currentYear = new Date().getFullYear();
       const range = 13;
       for (let i = 0; i < range; i++) {
         this.years.push(currentYear + i);
       }
-
+ 
     const totalPriceData = localStorage.getItem('totalPrice');
     const email = localStorage.getItem('email');
     }
     // // Dynamically generate years
     // const currentYear = new Date().getFullYear();
-    // const range = 13; 
+    // const range = 13;
     // for (let i = 0; i < range; i++) {
     //   this.years.push(currentYear + i);
     // }
    
-
-
+ 
+ 
 ngOnInit() {
   this.buyerDto = JSON.parse(localStorage.getItem('buyerDto') || '{}');
-  
+ 
   const totalPriceData = localStorage.getItem('totalPrice');
   if (totalPriceData) {
     this.totalCartValue = JSON.parse(totalPriceData);
   }
-
-
-
+ 
+ 
+ 
   this.checkoutForm = this.formBuilder.group({
     name: ['', Validators.required],
     cardNumber: ['', [Validators.required,cardNumberValidator]],
     CVV: ['', [Validators.required,CVVValidator]],
     address: ['', Validators.required],
     phoneNo: ['', Validators.required],
-    
-    
+   
+   
   });
-
-
+ 
+ 
 }
-
-
-
+ 
+ 
+ 
 makePayment() {
   this.paymentService.addPayment(this.payment).subscribe(
     (response) => {
@@ -120,13 +120,12 @@ onSubmit(): void {
   this.buyerDto = JSON.parse(localStorage.getItem('buyerDto') || '{}');
   if (this.checkoutForm.valid) {
     const paymentData: PaymentDto = {
-      paymentId: 0,
+      paymentId: 0 ,
       name: this.checkoutForm.value.name,
       email: this.buyerDto.email,
       address: this.checkoutForm.value.address,
       phoneNo: this.checkoutForm.value.phoneNo,
-      totalCartValue: this.totalCartValue,
-     
+      totalCartValue: this.totalCartValue
     };
     this.paymentService.addPayment(paymentData).subscribe(() => {
       //alert('Payment done');
@@ -147,9 +146,17 @@ onSubmit(): void {
       });
      
     },
-    
+    (error) => {
+      // Payment service
+     
+      const errorMessage = error?.error?.message || 'Unknown error occurred';
+      alert(errorMessage);
+      console.log(errorMessage);
+    }
+   
   );
 } else {
   // Form is invalid
   alert('Payment Failed.');
 }}}
+ 
